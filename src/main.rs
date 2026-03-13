12b2f321d8769d7443db94457524efba603fc382
Hamza <official.hamzaderaoui@gmail.com>
2026-03-13 06:59:05 +0000

    struct intialisation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use std::path::{PathBuf, Path};
+use clap::{Parser};
+
+mod node;
+mod create;
+
+#[derive(Parser, Debug)]  
+struct Args {
+    #[arg(default_value = ".")]
+
+    path: PathBuf
 }
+
+fn main() {
+    // let args = Args::parse();
+    // let absolute_path = std::fs::canonicalize(&args.path)
+    //     .expect("Failed to resolve path");
+    // 
+    // println!("Absolute path: {:?}", absolute_path);
+}
+
---
+++ src/node.rs
@@ -0,0 +1,13 @@
+#![allow(dead_code)]
+
+use std::path::PathBuf;
+
+struct Node<'a> {
+    parent: PathBuf,
+    kind: NodeKind<'a>
+}
+
+enum NodeKind<'a> {
+    File {name: &'a str},
+    Folder {name: &'a str, children:Vec<Node<'a>>}
+ }
